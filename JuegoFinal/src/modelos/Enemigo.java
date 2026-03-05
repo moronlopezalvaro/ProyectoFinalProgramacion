7bd7a676c9553ec8bfa9f0c0616d40d18219c34c
dariorumi5 <dariorumi5@alumnos.ilerna.com>
2026-03-05 12:27:49 +0100

    enemigo hecho

--- JuegoFinal/src/modelos/Enemigo.java
+++ JuegoFinal/src/modelos/Enemigo.java
@@ -1,6 +1,19 @@
-package modelos;
+package Modelos;
 
 public class enemigo {
+    double x, y;
+    int velocidad, ancho, alto;
 
+    public enemigo(double x, double y, int velocidad, int ancho, int alto) {
+        this.x = x;
+        this.y = y;
+        this.velocidad = velocidad;
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+    
+    public Rectangle getBounds() {
+        return new Rectangle((int)x, (int)y, ancho, alto);
+    }
 }
 
